45240e396c02620b0613951d03c6b8bdbde5ff93
ukkiwisurfer <uk.kiwisurfer@gmail.com>
2015-08-26 04:45:20 +0100

    Removed redundant project references. Added constructor to LogContainer that allows both typed and untyped arrays to be passed in.

--- source/Common.Logging/LogContainer.cs
+++ source/Common.Logging/LogContainer.cs
@@ -1,11 +1,11 @@
-using System;
-using Microsoft.SPOT;
 
 namespace Ignite.Infrastructure.Micro.Common.Logging
 {
     using System.Collections;
 
-    // <summary>
+    using Ignite.Infrastructure.Micro.Common.Assertions;
+
+    /// <summary>
     /// Container for multiple <see cref="LogEntry"/> elements.
     /// </summary>
     public class LogContainer
@@ -13,14 +13,14 @@ namespace Ignite.Infrastructure.Micro.Common.Logging
         /// <summary>
         /// Contains a collection of <see cref="LogEntry"/> items.
         /// </summary>
-        public ArrayList LogEntries { get; set; }
+        private readonly ArrayList m_LogEntries;
 
         /// <summary>
         /// Initialises an instance of the <see cref="LogContainer"/> class.
         /// </summary>
         public LogContainer()
         {
-            LogEntries = new ArrayList();
+            m_LogEntries = new ArrayList();
         }
 
         /// <summary>
@@ -31,14 +31,63 @@ namespace Ignite.Infrastructure.Micro.Common.Logging
         /// </param>
         public LogContainer(object[] items) : this()
         {
+            items.ShouldNotBeNull();
+
             foreach (var item in items)
             {
-                var entry = (LogEntry)item;
+                var entry = item as LogEntry;
                 if (entry != null)
                 {
-                    LogEntries.Add(entry);
+                    AddLogEntry(entry);
                 }
             }
+        }
+
+        /// <summary>
+        /// Initialises an instance of the <see cref="LogContainer"/> class.
+        /// </summary>
+        /// <param name="items">
+        /// The items to populate the container with.
+        /// </param>
+        public LogContainer(LogEntry[] items) : this()
+        {
+            items.ShouldNotBeNull();
+
+            foreach (var item in items)
+            {
+                AddLogEntry(item);
+            }
+        }
+
+        /// <summary>
+        /// Adds a new <see cref="LogEntry"/> to the container.
+        /// </summary>
+        /// <param name="entry">
+        /// A single log entry to be added to the container.
+        /// </param>
+        public void AddLogEntry(LogEntry entry)
+        {
+            if (entry != null)
+            {
+                m_LogEntries.Add(entry);
+            }
+        }
+
+        /// <summary>
+        /// Returns all the log entries managed by this container.
+        /// </summary>
+        /// <returns></returns>
+        public LogEntry[] GetLogEntries()
+        {
+            LogEntry[] items = new LogEntry[m_LogEntries.Count];
+
+            int index = 0;
+            foreach (var logItem in m_LogEntries)
+            {
+                items[index++] = (LogEntry) logItem;
+            }
+
+            return items;
         }
     }
 }
