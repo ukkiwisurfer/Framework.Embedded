ff88d0dfdd24c27d03aa31a00f2206305c816000
ukkiwisurfer <uk.kiwisurfer@gmail.com>
2015-09-19 22:45:59 +0100

    Initial commit of unit tests for framework.

---
+++ tests/Tests.Common.Assertions/BooleanAssertionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Tests.Common.Assertions
+{
+    using Ignite.Framework.Micro.Common.Assertions;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BooleanAssertionTests
+    {
+        [TestMethod]
+        public void ShouldBeTrue_ReturnsTrue_When_True()
+        {
+            BooleanAssertions.ShouldBeTrue(true);
+        }
+
+        [TestMethod]
+        public void ShouldBeTrue_Throws_When_False()
+        {
+            try
+            {
+                BooleanAssertions.ShouldBeTrue(false);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                // Assertion exception expected
+                e.OfType(typeof (ArgumentException));
+            }
+        }
+    }
+}
---
+++ tests/Tests.Common.Assertions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using MFUnitTest;
+
+namespace Tests.Common.Assertions
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // Run all tests in current assembly
+            TestManager.RunTests(Assembly.GetExecutingAssembly());
+
+            // Run all tests for specified Test Class
+            //TestManager.RunTest(typeof(BooleanAssertionTests));
+
+            // Run specified test for specified Test Class
+            //TestManager.RunTest(typeof(BooleanAssertionTests), "TestMethod1");
+        }
+    }
+}
