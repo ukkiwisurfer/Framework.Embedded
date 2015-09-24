cc315539442f319c20552af04b5e3608b07578f5
ukkiwisurfer <uk.kiwisurfer@gmail.com>
2015-09-24 13:41:21 +0100

    Added support to interrogate NTP server and set local time on device accordingly.

--- source/Common.Networking/Helpers/NetworkHelper.cs
+++ source/Common.Networking/Helpers/NetworkHelper.cs
@@ -1,6 +1,10 @@
 
 namespace Ignite.Framework.Micro.Common.Networking
-{  
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using Microsoft.SPOT.Hardware;
     using Microsoft.SPOT.Net.NetworkInformation;
 
     /// <summary>
@@ -61,6 +65,93 @@ namespace Ignite.Framework.Micro.Common.Networking
             }
 
             return information;
+        }
+
+        /// <summary>
+        /// Sets the local time on the device as the same as that derived from a NTP server.
+        /// </summary>
+        public void SetNetworkTime()
+        {
+            var networkTime = GetNetworkTime();
+            Utility.SetLocalTime(networkTime); 
+        }
+
+        /// <summary>
+        /// Retrives the time from a TNS server.
+        /// </summary>
+        /// <returns></returns>
+        public DateTime GetNetworkTime(string hostName = "time-a.nist.gov")
+        {
+            DateTime serverTime = new DateTime(1900, 1, 1);
+            DateTime networkDateTime = new DateTime(1900, 1, 1);
+
+            var hostEntry = Dns.GetHostEntry(hostName);
+            if (hostEntry.AddressList.Length > 0)
+            {
+                IPEndPoint endpoint = new IPEndPoint(hostEntry.AddressList[0],123);
+                using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+                {
+                    socket.Connect(endpoint);
+
+                    var ntpData = BuildNtpRequest();
+                    socket.Send(ntpData);
+                    socket.Receive(ntpData);
+
+                    networkDateTime = BuildNetworkTime(ntpData);
+                   
+                    socket.Close();
+                }
+            }
+
+            return networkDateTime;
+        }
+
+        /// <summary>
+        /// Parses the NTP response and constructs a network time
+        /// </summary>
+        /// <param name="ntpData"></param>
+        /// <returns></returns>
+        private DateTime BuildNetworkTime(byte[] ntpData)
+        {
+            var datetime = new DateTime(1900, 1, 1);
+
+            byte offsetTransmitTime = 40;
+            ulong intpart = 0;
+            ulong fractpart = 0;
+
+            for (int i = 0; i <= 3; i++)
+            {
+                intpart = 256 * intpart + ntpData[offsetTransmitTime + i];
+            }
+
+            for (int i = 4; i <= 7; i++)
+            {
+                fractpart = 256 * fractpart + ntpData[offsetTransmitTime + i];
+            }
+
+            ulong milliseconds = (intpart * 1000 + (fractpart * 1000) / 0x100000000L);
+            var timeSpan = TimeSpan.FromTicks((long)milliseconds * TimeSpan.TicksPerMillisecond);
+            datetime += timeSpan;
+
+            var offsetAmount = TimeZone.CurrentTimeZone.GetUtcOffset(datetime);
+            return (datetime + offsetAmount);
+        }
+
+        /// <summary>
+        /// Builds a NTP request.
+        /// </summary>
+        /// <returns></returns>
+        private byte[] BuildNtpRequest()
+        {
+            byte[] ntpData = new byte[48];
+            ntpData[0] = 0x1B;
+            
+            for (int index=1; index < 48; index++)
+            {
+                ntpData[index] = 0;
+            }
+
+            return ntpData;
         }
     }
 }
