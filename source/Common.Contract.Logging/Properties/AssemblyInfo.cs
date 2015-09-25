1aa7e15cd7e364acb490ac50cabeae86eb99a501
ukkiwisurfer <uk.kiwisurfer@gmail.com>
2015-09-25 09:54:00 +0100

    Use standard solution level assembly info details

--- source/Common.Contract.Logging/Properties/AssemblyInfo.cs
+++ source/Common.Contract.Logging/Properties/AssemblyInfo.cs
@@ -8,18 +8,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Common.Contract.Logging")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Common.Contract.Logging")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
