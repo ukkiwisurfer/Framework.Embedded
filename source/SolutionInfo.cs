a695a7cf2b3288f08b849feebb2ab252bf4d134d
ukkiwisurfer <uk.kiwisurfer@gmail.com>
2015-09-27 09:15:32 +0100

    Amend description for product attribute

--- source/SolutionInfo.cs
+++ source/SolutionInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Micro.Net Common Foundation")]
+[assembly: AssemblyProduct("A framework to simplify use of the .NET Micro on embedded controllers.")]
 [assembly: AssemblyCopyright("Copyright © Igniteous Limited 2014-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyVersion("0.2.0.0")]
