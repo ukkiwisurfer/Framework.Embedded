55eaaf990a01a0c2bf5a50b196c741739561a346
ukkiwisurfer <uk.kiwisurfer@gmail.com>
2015-10-19 10:18:12 +0100

    Updated assembly details across solution.

--- source/SolutionInfo.cs
+++ source/SolutionInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("A framework to simplify use of the .NET Micro on embedded controllers.")]
+[assembly: AssemblyProduct("A framework to simplify use of the .NET Micro on embedded devices.")]
 [assembly: AssemblyCopyright("Copyright © Igniteous Limited 2014-2015")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
