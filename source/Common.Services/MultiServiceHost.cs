110245b34fd01dcc156459a581b39c25ccc69124
ukkiwisurfer <uk.kiwisurfer@gmail.com>
2015-10-30 00:10:51 +0000

    Defensively check to see if service name already exists.

--- source/Common.Services/MultiServiceHost.cs
+++ source/Common.Services/MultiServiceHost.cs
@@ -141,10 +141,17 @@ namespace Ignite.Framework.Micro.Common.Services
         {
             service.ShouldNotBeNull();
 
-            var entry = new ServiceEntry(service.ServiceName, service); 
-            m_ServiceEntries.Add(entry.ServiceName, entry);
+            var entry = new ServiceEntry(service.ServiceName, service);
+            if (GetServiceByName(entry.ServiceName) == null)
+            {
+                m_ServiceEntries.Add(entry.ServiceName, entry);
+                m_Logger.Info(m_ResourceLoader.GetString(Resources.StringResources.AddedNewHost), service.ServiceId);
+            }
+            else
+            {
+                // Throw exception
+            }
 
-            m_Logger.Info(m_ResourceLoader.GetString(Resources.StringResources.AddedNewHost), service.ServiceId );
         }
 
         /// <summary>
